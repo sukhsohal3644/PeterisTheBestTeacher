3620e0ab3a305e07834a51a525d880a531d0d942
sukhsohal3644 <41071247+sukhsohal3644@users.noreply.github.com>
2019-01-17 17:08:19 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0731586_Week_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = new List<string>();
+
+            dogs.Add("Bichon");
+            dogs.Add("Poodle");
+            dogs.Add("Retriever");
+
+
+
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog + " ");
+            }
+
+
+            Console.WriteLine(dogs[1]);
+        }
+    }
+}
